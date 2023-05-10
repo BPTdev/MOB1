db051e34c8e7c31e9de71aacfd50a388843581b5
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-05-10 10:37:47 +0200

    xylo ok

--- xylofone/lib/main.dart
+++ xylofone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xylofone/widgets/xylofone_key.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Xylofone'),
     );
   }
 }
@@ -26,14 +28,15 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   int _height = 100;
   int _size = 40;
+  Color _iconColor = Color.fromRGBO(119, 119, 119, 1.0);
 
   @override
   Widget build(BuildContext context) {
@@ -47,46 +50,60 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             XylofonKey(
-              iconSize: _size,
-              sizedBoxSize: _height,
-              keyText: 'C',
-              keyColor: Color.fromRGBO(255, 0, 0, 1.0),
-            ),
+                iconSize: _size,
+                iconColor: _iconColor,
+                sizedBoxSize: _height,
+                keyText: 'C',
+                keyColor: Color.fromRGBO(255, 0, 0, 1.0),
+              audioNumber: 1,
+              ),
             XylofonKey(
               iconSize: _size,
+              iconColor: _iconColor,
               sizedBoxSize: _height,
               keyText: 'D',
               keyColor: Color.fromRGBO(255, 153, 0, 1.0),
+              audioNumber: 2,
             ),
             XylofonKey(
               iconSize: _size,
+              iconColor: _iconColor,
               sizedBoxSize: _height,
               keyText: 'E',
               keyColor: Color.fromRGBO(255, 250, 0, 1.0),
+              audioNumber: 3,
             ),
             XylofonKey(
               iconSize: _size,
+              iconColor: _iconColor,
               sizedBoxSize: _height,
               keyText: 'F',
               keyColor: Color.fromRGBO(17, 255, 0, 1.0),
+              audioNumber: 4,
             ),
             XylofonKey(
               iconSize: _size,
+              iconColor: _iconColor,
               sizedBoxSize: _height,
               keyText: 'G',
               keyColor: Color.fromRGBO(16, 143, 0, 1.0),
+              audioNumber: 5,
             ),
             XylofonKey(
               iconSize: _size,
+              iconColor: _iconColor,
               sizedBoxSize: _height,
               keyText: 'A',
               keyColor: Color.fromRGBO(0, 182, 255, 1.0),
+              audioNumber: 6,
             ),
             XylofonKey(
               iconSize: _size,
+              iconColor: _iconColor,
               sizedBoxSize: _height,
               keyText: 'B',
               keyColor: Color.fromRGBO(238, 0, 255, 1.0),
+              audioNumber: 7,
             ),
           ],
         ),
--- xylofone/lib/widgets/xylofone_key.dart
+++ xylofone/lib/widgets/xylofone_key.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class XylofonKey extends StatelessWidget {
   final int iconSize;
+  final Color iconColor;
   final int sizedBoxSize;
   final String keyText;
   final Color keyColor;
+  final int audioNumber;
 
   const XylofonKey({
     Key? key,
     required this.iconSize,
+    required this.iconColor,
     required this.sizedBoxSize,
     required this.keyText,
     required this.keyColor,
+    required this.audioNumber,
   }) : super(key: key);
 
+  void playSound(audioPath) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$audioNumber.wav'));
+  }
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery
-          .of(context)
-          .size
-          .width * 0.95,
-      height: sizedBoxSize.toDouble(),
-      child: Container(
+    return GestureDetector(
+      onTap: ()  {
+        playSound(audioNumber);
+      },
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.95,
+        height: sizedBoxSize.toDouble(),
+        child: Container(
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: keyColor,
+            borderRadius: BorderRadius.circular(10.0),
+            color: keyColor,
           ),
-          child:
-          Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
                 padding: EdgeInsets.only(left: 8.0),
                 child: Icon(
                   Icons.circle,
-                  color: Color.fromRGBO(119, 119, 119, 1.0),
+                  color: iconColor,
                   size: iconSize.toDouble(),
                 ),
               ),
@@ -50,12 +59,13 @@ class XylofonKey extends StatelessWidget {
                 padding: EdgeInsets.only(right: 8.0),
                 child: Icon(
                   Icons.circle,
-                  color: Color.fromRGBO(119, 119, 119, 1.0),
+                  color: iconColor,
                   size: iconSize.toDouble(),
                 ),
               ),
             ],
           ),
+        ),
       ),
     );
   }
