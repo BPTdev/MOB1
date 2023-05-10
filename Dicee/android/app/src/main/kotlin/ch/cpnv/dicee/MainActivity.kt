e9f5a12e75b0a109e981ee4e290810b8167feaa7
Benoit <benoit.pierrehumbert.pro@gmail.com>
2023-05-10 11:11:20 +0200

    Dicee : add the images

---
+++ Dicee/android/app/src/main/kotlin/ch/cpnv/dicee/MainActivity.kt
@@ -0,0 +1,6 @@
+package ch.cpnv.dicee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Dicee/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Center(
+          child: Text(
+            'Dicee',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image(
+                  image: AssetImage('assets/images/dice1.png'),
+                  height: MediaQuery.of(context).size.width * 0.4,
+                ),
+                Image(
+                  image: AssetImage('assets/images/dice1.png'),
+                  height: MediaQuery.of(context).size.width * 0.4,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
